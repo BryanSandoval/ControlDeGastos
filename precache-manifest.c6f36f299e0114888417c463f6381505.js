f48b28a7c81bee9ae07b2661466b88bfd4abee67
bryanSandoval <bchas831@gmail.com>
2021-11-11 23:20:48 -0600

    Updates

---
+++ precache-manifest.c6f36f299e0114888417c463f6381505.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bef961a4f8e981d715284ce3210a96d",
+    "url": "/ControlDeGastos/index.html"
+  },
+  {
+    "revision": "c4378d38509b5fa632b3",
+    "url": "/ControlDeGastos/static/css/main.571e6405.chunk.css"
+  },
+  {
+    "revision": "bd6bd55d0f17c3088198",
+    "url": "/ControlDeGastos/static/js/2.b190f742.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/ControlDeGastos/static/js/2.b190f742.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4378d38509b5fa632b3",
+    "url": "/ControlDeGastos/static/js/main.1e3a0cfe.chunk.js"
+  },
+  {
+    "revision": "e36f210fd7fd28c4e04d",
+    "url": "/ControlDeGastos/static/js/runtime-main.fbe88523.js"
+  }
+]);
